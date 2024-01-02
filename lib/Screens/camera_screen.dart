0f5269290430c5bb6ac24b4a18fb6a070b1ae6ab
AyStarc <21ucc030@lnmiit.ac.in>
2024-01-03 02:31:08 +0530

    [ERROR:flutter/runtime/dart_vm_initializer.cc(41)] Unhandled Exception: PlatformException(Failed to run model, Interpreter busy, java.lang.RuntimeException: Interpreter busy

--- lib/Screens/camera_screen.dart
+++ lib/Screens/camera_screen.dart
@@ -2,6 +2,8 @@ import 'package:btp_app/Screens/camera_screen.dart';
 import 'package:btp_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
+// import 'package:tflite/tflite.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -9,42 +11,72 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
+  late CameraController cameraController;
+  late CameraImage imgCamera;
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/mobilenet_v1_1.0_224.tflite",
+        labels: "assets/mobilenet_v1_1.0_224.txt");
+  }
 
   @override
   void initState() {
     super.initState();
-    _controller = CameraController(
+    initCamera();
+  }
+
+  initCamera() {
+    loadModel();
+    cameraController = CameraController(
       cameras[0],
       ResolutionPreset.medium,
     );
 
-    _initializeControllerFuture = _controller.initialize();
+    cameraController.initialize().then((value) {
+      if (mounted) {
+        setState(() {
+          cameraController.startImageStream((imageFromStream) {
+            imgCamera = imageFromStream;
+            runModelOnStreamFrames();
+          });
+        });
+      }
+    });
+  }
+
+  runModelOnStreamFrames() async {
+    var recognitions = await Tflite.runModelOnFrame(
+      bytesList: imgCamera.planes.map((plane) {
+        return plane.bytes;
+      }).toList(),
+      imageHeight: imgCamera.height,
+      imageWidth: imgCamera.width,
+      numResults: 3,
+      threshold: 0.2,
+    );
+
+    var result = "";
+
+    recognitions?.forEach((response) {
+      result +=
+          response["label"] + (response["confidence"] as double).toString();
+      print(result);
+    });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    cameraController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Camera Screen'),
-      ),
-      body: FutureBuilder(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Center(child: CameraPreview(_controller));
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Camera Screen'),
+        ),
+        body: Container(child: CameraPreview(cameraController)));
   }
 }
